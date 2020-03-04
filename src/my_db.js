6b0abd6f0d5bbaf6a757db4e9e496834eb85db11
kfleury <killian.fleury@epitech.eu>
2020-03-04 18:03:11 +0100

    [add] all my db

--- src/my_db.js
+++ src/my_db.js
@@ -5,29 +5,31 @@
 ** File description:
 ** js
 */
-//export class MyDB {}
 const fs = require('fs').promises;
 
 const schemaUsers = {
     users: [],
-    passwd: [],
+    account: [],
 };
 
-/*export default */class myDb {
+/*export default */
+class myDb {
     constructor(json) {
-        json = schemaUsers;
         this.json = json;
     }
-    async save(path) {
-        await fs.writeFile(path, JSON.stringify(this.json), 'utf-8');
-    }
+
     static async load(path) {
-        let data = await fs.readFile(path,'utf-8');
+        let data = await fs.readFile(path, 'utf-8');
         data = JSON.parse(data);
         return (new myDb(data));
     }
+
+    async save(path) {
+        await fs.writeFile(path, JSON.stringify(this.json), 'utf-8');
+    }
+
     async createUser(firstname, lastname) {
-        if (this.json.users.filter(data => (data.firstname === firstname) && (data.lastname === lastname)).length  === 1) {
+        if (this.json.users.filter(data => (data.firstname === firstname) && (data.lastname === lastname)).length === 1) {
             console.error("Already exist");
             return;
         }
@@ -38,27 +40,28 @@ const schemaUsers = {
         this.json.users.push(user);
         return (user);
     }
+
     async getUser(id) {
         if (!this.json.users[id]) {
             console.error("This id isn't exist");
-            return;
+
         } else return (this.json.users[id]);
     }
+
     async updateUser(id, firstname, lastname) {
         if (!this.json.users[id]) {
             console.error("This id isn't exist");
-            return;
-        }
-        else if (this.json.users.filter(data => (data.firstname === firstname) && (data.lastname === lastname)).length  === 1) {
+
+        } else if (this.json.users.filter(data => (data.firstname === firstname) && (data.lastname === lastname)).length === 1) {
             console.error("Already exist");
-            return;
-        }
-        else {
+
+        } else {
             this.json.users[id].firstname = firstname;
             this.json.users[id].lastname = lastname;
             return (this.json.users[id]);
         }
     }
+
     async deleteUser(id) {
         if (!this.json.users[id]) {
             console.error("This id isn't exist");
@@ -66,18 +69,88 @@ const schemaUsers = {
         }
         return (this.json.users.splice(id, 1));
     }
+
+    async createAccount(owner, initialBalance) {
+        if ((isNaN(initialBalance) === true) || (initialBalance <= 0)) {
+            console.error("InitialBalance is undefined");
+            return;
+        }
+        if (!this.json.users[owner]) {
+            console.error("This id isn't exist");
+            return;
+        }
+        if (this.json.account.filter(data => data.owner_id === owner).length === 1) {
+            console.error("You already got an account");
+            return;
+        }
+        const account = {
+            owner_id: owner,
+            initialBalance: initialBalance,
+        };
+        return (this.json.account.push(account));
+    }
+
+    async getAccount(id) {
+        if (!this.json.account[id]) {
+            console.error("This id isn't exist");
+            return;
+        }
+        return (this.json.account[id]);
+    }
+
+    async creditAccount(id, amount) {
+        if (!this.json.account[id]) {
+            console.error("This id isn't exist");
+            return;
+        }
+        if (amount <= 0) {
+            console.error("You can't add >= 0");
+            return;
+        }
+        return (this.json.account[id].initialBalance = this.json.account[id].initialBalance + amount);
+    }
+
+    async withdrawAccount(id, amount) {
+        if (!this.json.account[id]) {
+            console.error("This id isn't exist");
+            return;
+        }
+        if (amount <= 0) {
+            console.error("You can't add >= 0");
+            return;
+        }
+        return (this.json.account[id].initialBalance = this.json.account[id].initialBalance - amount);
+    }
+
+    async deleteAccount(id) {
+        if (!this.json.account[id]) {
+            console.error("This id isn't exist");
+            return;
+        }
+        return (this.json.account.splice(id, 1));
+    }
 }
 
-/*async function main() {
-    let re = new myDb;
+async function Users(re) {
     await re.createUser("Killian", "Fleury");
     await re.createUser("Edouard", "Sen");
     await re.createUser("Olivier", "Mzi");
-    await re.createUser("Tom", "Tbn");
+    await re.createUser("Tom", "Laqueue");
     await re.updateUser(1, "Maxime", "Dodin");
-    await re.deleteUser(60);
-    console.log(re.json.users);
-    console.log();
+}
+
+async function Account(re) {
+    await re.createAccount('a', 10000);
+    await re.createAccount(1, 10000);
+    await re.creditAccount(0, 5000);
+    await re.withdrawAccount(0, 2500);
+}
+
+/*async function main() {
+    let re = new myDb(schemaUsers);
+    await Users(re);
+    await Account(re);
+    console.log(re.json.account);
 }
 
 main();*/
