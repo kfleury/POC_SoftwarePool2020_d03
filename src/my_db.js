a9d185bb6cbaddb7e411f4795f85f10311234113
kfleury <killian.fleury@epitech.eu>
2020-03-04 17:15:32 +0100

    [add] Gestion des utilisateurs dans la classe myDb

--- src/my_db.js
+++ src/my_db.js
@@ -8,8 +8,14 @@
 //export class MyDB {}
 const fs = require('fs').promises;
 
-export default class myDb {
+const schemaUsers = {
+    users: [],
+    passwd: [],
+};
+
+/*export default */class myDb {
     constructor(json) {
+        json = schemaUsers;
         this.json = json;
     }
     async save(path) {
@@ -20,16 +26,58 @@ export default class myDb {
         data = JSON.parse(data);
         return (new myDb(data));
     }
+    async createUser(firstname, lastname) {
+        if (this.json.users.filter(data => (data.firstname === firstname) && (data.lastname === lastname)).length  === 1) {
+            console.error("Already exist");
+            return;
+        }
+        const user = {
+            firstname: firstname,
+            lastname: lastname,
+        };
+        this.json.users.push(user);
+        return (user);
+    }
+    async getUser(id) {
+        if (!this.json.users[id]) {
+            console.error("This id isn't exist");
+            return;
+        } else return (this.json.users[id]);
+    }
+    async updateUser(id, firstname, lastname) {
+        if (!this.json.users[id]) {
+            console.error("This id isn't exist");
+            return;
+        }
+        else if (this.json.users.filter(data => (data.firstname === firstname) && (data.lastname === lastname)).length  === 1) {
+            console.error("Already exist");
+            return;
+        }
+        else {
+            this.json.users[id].firstname = firstname;
+            this.json.users[id].lastname = lastname;
+            return (this.json.users[id]);
+        }
+    }
+    async deleteUser(id) {
+        if (!this.json.users[id]) {
+            console.error("This id isn't exist");
+            return;
+        }
+        return (this.json.users.splice(id, 1));
+    }
 }
 
 /*async function main() {
-    const db = {
-        login: "Shinsei",
-        passwd: "H4ck3rmAn",
-    };
-    const path = process.argv[2];
-    let re = await myDb.load(path);
-    console.log(re);
+    let re = new myDb;
+    await re.createUser("Killian", "Fleury");
+    await re.createUser("Edouard", "Sen");
+    await re.createUser("Olivier", "Mzi");
+    await re.createUser("Tom", "Tbn");
+    await re.updateUser(1, "Maxime", "Dodin");
+    await re.deleteUser(60);
+    console.log(re.json.users);
+    console.log();
 }
 
 main();*/
