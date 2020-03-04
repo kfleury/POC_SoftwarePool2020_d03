a0c20049cd105d456d1a9cba5dec81ec30bbd373
kfleury <killian.fleury@epitech.eu>
2020-03-04 14:48:18 +0100

    [add] class MyDB with save and load function

--- src/my_db.js
+++ src/my_db.js
@@ -5,9 +5,33 @@
 ** File description:
 ** js
 */
+//export class MyDB {}
+const fs = require('fs').promises;
 
-function main() {
-
+export default class myDb {
+    constructor(json) {
+        this.json = json;
+    }
+    async save(path) {
+        await fs.writeFile(path, JSON.stringify(this.json), 'utf-8');
+    }
+    static async load(path) {
+        let data = await fs.readFile(path,'utf-8');
+        data = JSON.parse(data);
+        return (new myDb(data));
+    }
 }
 
-main();
+/*async function main() {
+    const db = {
+        login: "Shinsei",
+        passwd: "H4ck3rmAn",
+    };
+    const path = process.argv[2];
+    let re = await myDb.load(path);
+    console.log(re);
+}
+
+main();*/
+
+
